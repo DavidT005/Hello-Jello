0dd93270ee0fbb323085f87a043f0dccb0f8d56a
David Tobon <davidtobon.dat@gmail.com>
2021-11-30 17:02:16 -0500

    Updated HailManager script so the hail drop point goes up in time; implemented cinemachine too

--- Hello Jello/Assets/Scripts/HailManager.cs
+++ Hello Jello/Assets/Scripts/HailManager.cs
@@ -7,6 +7,8 @@ public class HailManager : MonoBehaviour
 
     public GameObject hail; 
     public float hailWaitTime;  // The time between each hail
+    public Vector2 dropPos = new Vector2(0,5);
+    public float zenithSpeed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +19,18 @@ public class HailManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateDropPos();
     }
 
     void SpawnHail()
     {
-        float xHailPos = Random.Range(-10f,10f);
-        GameObject newHail = Instantiate(hail, new Vector2(xHailPos,5), transform.rotation);
+        GameObject newHail = Instantiate(hail, dropPos, transform.rotation);
         newHail.transform.parent = transform;
-        
+    }
+
+    void UpdateDropPos()
+    {
+        dropPos = new Vector2(Random.Range(-10f,10f), dropPos.y + zenithSpeed*Time.deltaTime);
     }
 
 
