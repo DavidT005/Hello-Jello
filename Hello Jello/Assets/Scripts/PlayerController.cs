1c68010c4589676a1365bcbdcf1e100a0475e3f6
David Tobon <davidtobon.dat@gmail.com>
2021-11-30 12:28:31 -0500

    Implemented jump and started ground check method

--- Hello Jello/Assets/Scripts/PlayerController.cs
+++ Hello Jello/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,27 @@ public class PlayerController : MonoBehaviour
 
     Vector3 moveDirection;   // To store controller input
     [SerializeField] int playerSpeed = 15;   // How fast will player move
+    public float jumpMagnitude = 10f;  //How much is player going to jump
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = transform.GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        Jump();
+        CheckObjectBelow();
+
+
+    }
+
+    void Move()
     {
         // We get platform-independet user input
         moveDirection.x = Input.GetAxis("Horizontal");
@@ -23,7 +35,30 @@ public class PlayerController : MonoBehaviour
 
         // Then we update the player's position
         transform.position += moveDirection * Time.deltaTime * playerSpeed; //Updates position, Time.deltaTime is time between frames
-
-        
     }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += new Vector2(rb.velocity.x, jumpMagnitude);
+        }
+    }
+
+    GameObject CheckObjectBelow()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.down, 5);
+
+        if(hit.collider != null)
+        {
+            print(hit.collider.gameObject.name);
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
 }
